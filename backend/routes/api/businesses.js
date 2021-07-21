f6bbb85a435d3bf773c63c803a72b1b3f0265298
kyle <ktseng122@gmail.com>
2021-07-21 02:03:59 -0400

    Backend api routes check

--- backend/routes/api/businesses.js
+++ backend/routes/api/businesses.js
@@ -16,4 +16,35 @@ router.get('/:id', asyncHandler(async (req, res) => {
   return res.json(oneBusiness);
 }))
 
+router.post('/', asyncHandler(async function (req, res) {
+  const data = req.body;
+  const business = await Business.create(data)
+  const newBusiness = await Business.findByPk(business.id, {
+    include: User
+  })
+
+  res.json(newBusiness)
+}))
+
+router.put('/:id', asyncHandler(async function (req, res) {
+  const id = req.body.id
+  delete req.body.id;
+
+  await Business.update(req.body,
+    { where: { id } }
+  )
+
+  const business = await Business.findByPk(id)
+  return res.json(business)
+}))
+
+
+router.delete('/:id', asyncHandler(async function (req, res) {
+  const businessId = req.params.id
+  const business = await Business.findByPk(businessId);
+
+  const deletedBusiness = await Business.destroy({ where: { id: businessId } })
+
+}))
+
 module.exports = router;
