c49904616a11ce9d9c8ef8b18b3ecba56832a867
kyle <ktseng122@gmail.com>
2021-07-22 03:13:08 -0400

    Set up css layout for individual business pages

--- frontend/src/components/BusinessDetails/index.js
+++ frontend/src/components/BusinessDetails/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useParams, NavLink } from 'react-router-dom';
+import { useParams, NavLink, useHistory } from 'react-router-dom';
 import { Modal } from '../../context/Modal';
 
 import { getBusinesses, getBusinessDetails } from '../../store/business';
@@ -11,6 +11,7 @@ function BusinessDetails() {
   const { businessId } = useParams();
   const [showEditBusiness, setShowEditBusiness] = useState(false);
   const dispatch = useDispatch();
+  let history = useHistory();
   const sessionUser = useSelector(state => state.session.user?.id);
   const ownerId = useSelector(state => state.businesses.user?.id);
   const business = useSelector((state) => {
@@ -22,19 +23,39 @@ function BusinessDetails() {
     dispatch(getBusinessDetails(businessId))
   }, [businessId])
 
+  //Redirect to new review page
+  const handleClick = () => {
+    history.push(`/businesses/${businessId}/new-review`)
+  }
 
   return (
-    <div className="business-details">
+    <div className="business-container">
+      {/* contains "Write a review, Add a Photo and maybe a google maps?" */}
+      {/* <NavLink to={`/businesses/${business.id}`} className='titleLink'>{business?.title}</NavLink> */}
+      {/* <div className="business-box"> */}
+      <div className="business-details"></div>
+      <h1>{business?.title}</h1>
+      <p>"{business?.description}"</p>
+      {/* Button will probably have its own div to go along with Adding a photo and seeing all photos */}
       <div>
-        <div className="pad"></div>
-        <h1>{business?.title}</h1>
-        <p>{business?.description}</p>
-        <p>{business?.address}</p>
-        <p>{business?.city}</p>
-        <p>{business?.state}</p>
-        <p>{business?.zipCode}</p>
+        <button onClick={handleClick}>✩ Write a Review</button>
       </div>
       <div>
+        <h2>Location and Hours</h2>
+      </div>
+      <div className='address-box'>
+        <NavLink className='address-navlink' to={`/businesses/directions`}>Get Directions</NavLink>
+        <p>{business?.address}  </p>
+        <p>{business?.city}, {business?.state}, {business?.zipCode}</p>
+
+      </div>
+      <div>
+        <h2>Recommended Reviews</h2>
+      </div>
+      <div className='reviews-container'>
+
+      </div>
+      <div className='edit-button-div'>
         {showEditBusiness && (
           <Modal className='editModal' onClose={() => setShowEditBusiness(false)}>
             <EditBusinessForm setShowEditBusiness={setShowEditBusiness} id={business?.id} />
@@ -46,12 +67,10 @@ function BusinessDetails() {
           Edit Business
         </button>
       </div>
-      <div>
-        <NavLink to={`/businesses/${business?.id}/new-review`}>
-          Leave a review!
-        </NavLink>
+      <div className='location-hours'>
+
       </div>
-    </div>
+    </div >
   )
 }
 
